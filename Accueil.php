5a2fd306b83021525fc7e772c07b16238562eba9
XuanWEI24 <mllexuanwei@gmail.com>
2020-06-02 16:41:47 +0800

    Pagination 2

--- Accueil.php
+++ Accueil.php
@@ -120,55 +120,7 @@
             <div id="page_navigation"> </div>
          </div>
            
-            <script>
 
-                var show_per_page = 5;
-                var current_page = 0;
-
-                function set_display(first, last) {
-                    $('#cartes').children().css('display', 'none');
-                    $('#cartes').children().slice(first, last).css('display', 'block');
-                    
-                }
-
-                function previous(){
-                    if($('.active').prev('.page_link').length) go_to_page(current_page - 1);
-                }
-
-                function next(){
-                    if($('.active').next('.page_link').length) go_to_page(current_page + 1);
-                }
-
-                function go_to_page(page_num){
-                    current_page = page_num;
-                    start_from = current_page * show_per_page;
-                    end_on = start_from + show_per_page;
-                    set_display(start_from, end_on);
-                    $('.active').removeClass('active');
-                    $('#id' + page_num).addClass('active');
-                }
-
-                $(document).ready(function() {
-
-                    var number_of_pages = Math.ceil($('#cartes').children().length / show_per_page);
-
-                    var nav = '<nav aria-label="Page navigation example" class="page"><ul class="pagination justify-content-center"><li class="page-item"><a class="page-link" href="javascript:previous();">Précédent</a>';
-
-                    var i = -1;
-                    while(number_of_pages > ++i){
-                        nav += '<li class="page_link'
-                        if(!i) nav += ' active';
-                        nav += '" id="id' + i +'">';
-                        nav += '<a class="page-link" href="javascript:go_to_page(' + i +')">'+ (i + 1) +'</a>';
-                    }
-                    nav += '<li class="page-item"><a class="page-link" href="javascript:next();">Suivant</a></ul></nav>';
-
-                    $('#page_navigation').html(nav);
-                    set_display(0, show_per_page);
-
-                });
-
-            </script>
             <!--------------------------------------------------------------------------------------------------------------------------------------------->
           <div class="container" id="talents">
               <h1 id="titre2"><a href="Talent.php" class="badge badge-light">Talents</a></h1><br>
@@ -208,23 +160,88 @@
                         }  
             	?>
             </div>            
-             <nav aria-label="Page navigation example" class="page">
-              <ul class="pagination justify-content-center">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1">Précédent</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">Suivant</a>
-                </li>
-              </ul>
-            </nav>
-          
-           
-           
+             <div id="page_navigation2"> </div>     
           </div>
+            <script>
+
+                var show_per_page = 5;
+                var current_page = 0;
+
+                function set_display(first, last) {
+                    $('#cartes').children().css('display', 'none');
+                    $('#cartes').children().slice(first, last).css('display', 'block');
+                }
+                function set_display2(first, last) {
+                    $('#cartest').children().css('display', 'none');
+                    $('#cartest').children().slice(first, last).css('display', 'block');
+                }
+
+                function previous(){
+                    if($('.active').prev('.page_link').length) go_to_page(current_page - 1);
+                }
+
+                function next(){
+                    if($('.active').next('.page_link').length) go_to_page(current_page + 1);
+                }
+                function previous2(){
+                    if($('.active').prev('.page_link').length) go_to_page2(current_page - 1);
+                }
+
+                function next2(){
+                    if($('.active').next('.page_link').length) go_to_page2(current_page + 1);
+                }
+                function go_to_page(page_num){
+                    current_page = page_num;
+                    start_from = current_page * show_per_page;
+                    end_on = start_from + show_per_page;
+                    set_display(start_from, end_on);
+                    $('.active').removeClass('active');
+                    $('#id' + page_num).addClass('active');
+                }
+                function go_to_page2(page_num){
+                    current_page = page_num;
+                    start_from = current_page * show_per_page;
+                    end_on = start_from + show_per_page;
+                    set_display2(start_from, end_on);
+                    $('.active').removeClass('active');
+                    $('#sid' + page_num).addClass('active');
+                }
+                $(document).ready(function() {
+
+                    var number_of_pages = Math.ceil($('#cartes').children().length / show_per_page);
+                    var number_of_pages2 = Math.ceil($('#cartest').children().length / show_per_page);
+                    
+                    var nav = '<nav aria-label="Page navigation example" class="page"><ul class="pagination justify-content-center"><li class="page-item"><a class="page-link" href="javascript:previous();">Précédent</a>';
+                    var nav2 = '<nav aria-label="Page navigation example" class="page"><ul class="pagination justify-content-center"><li class="page-item"><a class="page-link" href="javascript:previous2();">Précédent</a>';
+                    
+                    var i = -1;
+                    while(number_of_pages > ++i){
+                        nav += '<li class="page_link'
+                        if(!i) nav += ' active';
+                        nav += '" id="id' + i +'">';
+                        nav += '<a class="page-link" href="javascript:go_to_page(' + i +')">'+ (i + 1) +'</a>';
+                    }
+                    nav += '<li class="page-item"><a class="page-link" href="javascript:next();">Suivant</a></ul></nav>';
+
+                    $('#page_navigation').html(nav);
+                    set_display(0, show_per_page);
+
+                    var i = -1;
+                    while(number_of_pages2 > ++i){
+                        nav2 += '<li class="page_link'
+                        if(!i) nav2 += ' active';
+                        nav2 += '" id="sid' + i +'">';
+                        nav2 += '<a class="page-link" href="javascript:go_to_page2(' + i +')">'+ (i + 1) +'</a>';
+                    }
+                    nav2 += '<li class="page-item"><a class="page-link" href="javascript:next2();">Suivant</a></ul></nav>';
+
+                    $('#page_navigation2').html(nav2);
+                    set_display2(0, show_per_page);
+
+                });
+
+            </script>
+            
            
 <!--------------------------------------------------------------------------------------------------------------------------------------------->
      <!--    <div class="container" id="cours">
