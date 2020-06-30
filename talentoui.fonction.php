fc13b14a46ec13a89b402c65b8588e4433ee8d18
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-06-30 11:23:09 +0200

    email 3.0

--- talentoui.fonction.php
+++ talentoui.fonction.php
@@ -1,1 +1,8 @@
+<?php
+require_once 'Fonctions.php';
 
+$sql = "insert into compteurt (NumOuiT, NumNonT) VALUES(1, 0)";
+$result = mysqli_query ($session, $sql);  
+header("Location: Accueil.php");
+
+?>
