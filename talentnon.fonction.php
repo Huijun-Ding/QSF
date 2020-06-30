50e128b6647a94d47f6e6dc275c330407b498eb2
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-06-30 09:48:37 +0200

    probleme talentnon.html.php

--- talentnon.fonction.php
+++ talentnon.fonction.php
@@ -1,10 +1,11 @@
 <?php
 require_once 'Fonctions.php';
-$raisonT = $_GET['raison_non_talent'];
+$raisonT = $_GET['raison_non_talent'].$_GET['datedispo'].$_GET['autre_raison'];
 
-if (isset($raisonB)) {
+if (isset($raisonT)) {
     $sql = "insert into compteurt (NumOuiT, NumNonT, RaisonT) VALUES(0, 1, '{$raisonT}')";
     $result = mysqli_query ($session, $sql);  
-    header("Location: Accueil.php");
+    //header("Location: Accueil.php");
+    echo $raisonT;
 }
 ?>
--- talentnon.html.php
+++ talentnon.html.php
@@ -1,8 +1,120 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+​
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+​	<link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Quai des savoir-faire</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://cdn.ckeditor.com/4.14.1/standard/ckeditor.js"></script>
+  </head>
+  <body>
+       <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="Accueil.php">Quai des savoir-faire</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="Accueil.php">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Besoin.php">Besoins</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Talent.php">Talents</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="AbonnerCategorie.php">Catégories</a>
+          </li>  
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item dropdown">   
+            <?php
+            require_once 'Fonctions.php';
+            
+            if(isset($_SESSION['email'])){
+                    echo('<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
+                    echo $_SESSION['email'];       // quand l'utiliateur n'a pas croché le case Anonyme au moment de l'inscription, on va afficher son adresse mail
+                    echo('</a>');
+            } else {
+                echo('<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
+                echo "Visiteur";                   //Utilisateur qui n'a pas conncté
+                echo('</a>');
+            } 
+            ?>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <?php
+                if(isset($_SESSION['email'])){
+                    echo ('<a class="dropdown-item" href="MonProfil.php">Mon profil</a>');
+                    echo ('<a class="dropdown-item" href="MesCategories.php">Mes catégories</a>');
+                    echo ('<a class="dropdown-item" href="Deconnecter.php" onclick="Deconnexion()">Déconnecter</a>');
+                ?>
+                    <script>
+                        function Deconnexion() {
+                            alert("Déconnexion réussite !");
+                            }
+                    </script>
+                <?php
+                } else {
+                    echo ('<a class="dropdown-item" href="Login.php">Se connecter</a>');
+                    echo ('<a class="dropdown-item" href="Inscription.php">S\'inscrire</a>');
+                }
+                ?>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+<!--------------------------------------------------------------------------------------------------------------------------------------------->
+        <div class="jumbotron">
+          <div class="container">
+              <h1>Pourquoi ?</h1><hr>
+              <p>Veuillez sélectionner une raison de refuse : </p><br>
+              <form action="talentnon.fonction.php" method="GET">
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="raison_non_talent" id="talent_raison1" value="Je ne suis pas libre" checked>
+                  <label class="form-check-label" for="talent_raison1">
+                    Je ne suis pas libre
+                  </label>
+                </div><br>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="raison_non_talent" id="talent_raison2" value="Je ne suis pas disponible jusqu'au ">
+                  <label class="form-check-label" for="talent_raison2">
+                    Je ne suis pas disponible jusqu'au 
+                  </label>
+                </div><br>
+                <input type="date" name="datedispo">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="" id="talent_raison3" value="">
+                  <label class="form-check-label" for="talent_raison3">
+                    Autre raison (veuillez préciser) 
+                  </label><br>
+                  <textarea name="autre_raison" rows="4" cols="50"></textarea>
+                </div><br>
+                <button type="submit" class="btn btn-primary">Envoyer</button>
+              </form>
+              <hr>      
+          </div>
+        </div>
+
+        <footer>
+          <p id="copyright"><em><small>copyright &#9400; Quai des savoir-faire, CPAM Haute-Garonne, 2020. All rights reserved.</small></em></p>
+        </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
