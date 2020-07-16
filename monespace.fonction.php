a87163a4de96ed5379822a0c2a34a7b0802c0672
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-07-16 15:59:37 +0200

    change inscription

--- monespace.fonction.php
+++ monespace.fonction.php
@@ -2,16 +2,14 @@
 require_once 'Fonctions.php';
 
 $retype = $_POST['switch-two'];  //type choisi dans mon espace via la boutton radio
-/*
+
 $stmt = mysqli_prepare($session, 'UPDATE `utilisateurs` SET `TypeU` = ? WHERE `utilisateurs`.`CodeU` = ? ; ');   // Connecter et vérification de mot de passe
 mysqli_stmt_bind_param($stmt, "si", $retype, $usercode);
-mysqli_stmt_execute($stmt);
-header("Location: MonProfil.php");
-*/
 
-$query = "UPDATE `utilisateurs` SET `TypeU` = $retype WHERE `utilisateurs`.`CodeU` = $usercode ";
-if (mysqli_query ($session, $query) == TRUE) {
-    header("Location: MonProfil.php");
+if (mysqli_stmt_execute($stmt) == TRUE) {
+  header("Location: MonProfil.php");
+} else {
+    echo 'erreur';
 }
 
 ?>    
