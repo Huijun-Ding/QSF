e0c9b5848369fc948e67bbc02be416e57980a615
XuanWEI24 <mllexuanwei@gmail.com>
2020-07-24 19:58:21 +0800

    cartes emails en commentaires

--- AdminCarteInapproprieB.php
+++ AdminCarteInapproprieB.php
@@ -10,19 +10,7 @@ if (isset($_POST['desactiverb'])) {
     mysqli_stmt_execute($stmt1);
 }
 
-//Envoyer un mail pour informer cette personne
-        $Email = mysqli_prepare($session, "SELECT u.Email FROM utilisateurs u, saisir s WHERE u.CodeU = s.CodeU and CodeB = ?");   
-        mysqli_stmt_bind_param($Email, 'i', $CodeB);
-        mysqli_stmt_execute($Email);  
-        
-        $destinataire = "$Email"; // adresse mail du destinataire
-        $sujet = "Votre besoin a été supprimé par l'administrateur"; // sujet du mail
-        $message = "Votre besoin a été supprimé par l'administrateur à cause des contenus inappropriés"; // message qui dira que le destinataire a bien lu votre mail
-        // maintenant, l'en-tête du mail
-        $header = "From: [Quai des savoir-faire]\r\n"; 
-        $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
-        $header .= "Disposition-Notification-To:l'email d'un administrateur"; // c'est ici que l'on ajoute la directive
-        mail ($destinataire, $sujet, $message, $header); // on envois le mail    
+   
 
 
 
@@ -36,7 +24,21 @@ if (isset($_POST['activerb'])) {
 
 
 
-
 header("Location: Admin.php");
 
+//Envoyer un mail pour informer cette personne
+/*
+        $Email = mysqli_prepare($session, "SELECT u.Email FROM utilisateurs u, saisir s WHERE u.CodeU = s.CodeU and CodeB = ?");   
+        mysqli_stmt_bind_param($Email, 'i', $CodeB);
+        mysqli_stmt_execute($Email);  
+        
+        $destinataire = "$Email"; // adresse mail du destinataire
+        $sujet = "Votre besoin a été supprimé par l'administrateur"; // sujet du mail
+        $message = "Votre besoin a été supprimé par l'administrateur à cause des contenus inappropriés"; // message qui dira que le destinataire a bien lu votre mail
+        // maintenant, l'en-tête du mail
+        $header = "From: [Quai des savoir-faire]\r\n"; 
+        $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+        $header .= "Disposition-Notification-To:l'email d'un administrateur"; // c'est ici que l'on ajoute la directive
+        mail ($destinataire, $sujet, $message, $header); // on envois le mail 
+*/
 ?>
