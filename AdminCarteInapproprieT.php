dad3f1d01dd81e4ddb542a1713e16023297a9b7a
XuanWEI24 <mllexuanwei@gmail.com>
2020-07-24 19:52:25 +0800

    admin cartes

--- AdminCarteInapproprieT.php
+++ AdminCarteInapproprieT.php
@@ -10,6 +10,20 @@ if (isset($_POST['desactivert'])) {
 }
        
 //Envoyer un mail pour informer cette personne
+        $Email = mysqli_prepare($session, "SELECT u.Email FROM utilisateurs u, proposer p WHERE u.CodeU = p.CodeU and CodeT = ?");   
+        mysqli_stmt_bind_param($Email, 'i', $CodeT);
+        mysqli_stmt_execute($Email);  
+        
+        $destinataire = "$Email"; // adresse mail du destinataire
+        $sujet = "Votre talent a été supprimé par l'administrateur"; // sujet du mail
+        $message = "Votre talent a été supprimé par l'administrateur à cause des contenus inappropriés"; // message qui dira que le destinataire a bien lu votre mail
+        // maintenant, l'en-tête du mail
+        $header = "From: [Quai des savoir-faire]\r\n"; 
+        $headers = 'Content-Type: text/plain; charset=utf-8' . "\r\n";
+        $header .= "Disposition-Notification-To:l'email d'un administrateur"; // c'est ici que l'on ajoute la directive
+        mail ($destinataire, $sujet, $message, $header); // on envois le mail    
+
+
 
 $CodeTC = $_POST['activert'];
 
