be116560b0067fe6215a7bfa85cf79ecd06aae3c
XuanWEI24 <mllexuanwei@gmail.com>
2020-08-04 17:05:13 +0800

    emails

--- IdentificationUtilisateur.php
+++ IdentificationUtilisateur.php
@@ -9,7 +9,7 @@ if(isset($_POST['email'])){
     $sql = "SELECT RoleU from utilisateurs where Email = '{$Email}'";
     $result = mysqli_query ($session, $sql);
     if ($ligne = mysqli_fetch_array($result)) {   
-        if ($ligne['RoleU'] = 'admin') {
+        if ($ligne['RoleU'] == 'admin') {
             $stmt = mysqli_prepare($session, "SELECT MotDePasse FROM utilisateurs WHERE Email=?");   // Connecter et vérification de mot de passe
             mysqli_stmt_bind_param($stmt, "s", $Email);
             mysqli_stmt_execute($stmt);
