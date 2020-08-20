f498e9d27999fb1c434a24a432841ea8b0e26fc6
Huijun-Ding <mathilda.cnfr@gmail.com>
2020-08-20 16:16:35 +0200

    remember me

--- IdentificationUtilisateur.php
+++ IdentificationUtilisateur.php
@@ -4,14 +4,27 @@ require_once('Fonctions.php');
 if(isset($_POST['email'])){
     $Email = $_POST['email'];
     $Password = $_POST["password"];
+    $RememberMe = $_POST['remember'];
     
+    // se souvenir de moi
+    if ($RememberMe == 1) {     
+        setcookie('email',$Email,time()+3600);
+        setcookie('password',$Password,time()+3600);
+        setcookie('remember',$RememberMe,time()+3600);
+    } else {
+        setcookie('email',$Email,time()-3600);
+        setcookie('password',$Password,time()-3600);
+        setcookie('remember',$RememberMe,time()-3600);
+    }    
+    
+    // déterminer si c'est un admin
     $sql = "SELECT RoleU from utilisateurs where Email = '{$Email}'";
     $result = mysqli_query ($session, $sql);
     
     if (mysqli_num_rows($result)>0) {
       while ($ligne = mysqli_fetch_array($result)) {   
 
-        if (isset($ligne['RoleU'])) {
+        if (isset($ligne['RoleU'])) { 
             $stmt = mysqli_prepare($session, "SELECT MotDePasse FROM utilisateurs WHERE Email=?");   // Connecter et vérification de mot de passe
             mysqli_stmt_bind_param($stmt, "s", $Email);
             mysqli_stmt_execute($stmt);
